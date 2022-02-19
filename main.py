34e5e07ebde9561bb0d7478e6593b54fe34e372d
Asopos <rene.lehmann@stud.uni-due.de>
2022-02-19 20:57:35 +0100

    change filter for flats

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
+import re
 
 client = MongoClient(host="localhost", port=2717)
+
+detailURL = 'https://www.immowelt.de/expose/'
+flatListURL = 'https://www.immowelt.de/liste/duisburg/wohnungen/mieten?d=true&sd=DESC&sf=RELEVANCE&sp='
 
 immoPyDB = client.immoPy
 
@@ -11,15 +15,25 @@ def extract_flat_details(flat_soup):
     qm = flat_soup.select_one('div[data-test="area"]').string
     rooms = flat_soup.select_one('div[data-test="rooms"]').string
     landlord = flat_soup.select_one('div[class*="ProviderName"]').string
-    features = flat_soup.select('div[class*="IconFact"]')
-    location = features[0].findChildren('span')[0].get_text()
-    description = features[1].findChildren('span')[0].get_text()
+    if flat_soup.find("i", text="location"):
+        location = flat_soup.find("i", text="location").nextSibling.text.split(', ')
+        street = location[0]
+        district = location[1]
+
+    else:
+        location = ''
+    if flat_soup.find("i", text="check"):
+        description = flat_soup.find("i", text="check").nextSibling.text
+    else:
+        description = ''
     return {
         "Vermieter": landlord,
         "Preis": price.split(" ")[0],
         "Quadratmeter": qm.split(" ")[0],
         "Zimmer": rooms.split(" ")[0],
-        "Ort": location,
+        "Straße": re.sub(r'\d+', '', street).strip(),
+        "Hausnummer": re.findall('\d+', street)[0],
+        "Stadtteil": district,
         "Beschreibung": description
     }
 
@@ -36,5 +50,8 @@ if __name__ == '__main__':
 
     page = requests.get(url=url)
     page_result = BeautifulSoup(page.content, 'html.parser')
+    #subPages = page_result.select_one('div[class*="Pagination-"]')
+
     flats = get_flat_data(page_result)
-    immoPyDB.insert_many(flats)
+    print(flats)
+    #immoPyDB.insert_many(flats)
