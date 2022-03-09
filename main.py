5c0239366803572bc7026209149a4ad047c8fabc
Asopos <rene.lehmann@stud.uni-due.de>
2022-03-09 11:04:33 +0100

    Add HTML_Session for rendering webpage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from requests_html import HTMLSession
 from pymongo import MongoClient
 import re
 
@@ -40,18 +41,32 @@ def extract_flat_details(flat_soup):
 def get_flat_data(page_result):
     flat_list = []
     for flat in page_result.select('div[class*="EstateItem"]'):
-        flat_data = extract_flat_details(flat)
+        try:
+            flat_data = extract_flat_details(flat)
+        except Exception as err:
+            print('Oh no, there is an error...:', err)
+            continue
+        print(flat_data)
         flat_list.append(flat_data)
     return flat_list
 
-
 if __name__ == '__main__':
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
     url = 'https://www.immowelt.de/liste/duisburg/wohnungen/mieten?sort=relevanz'
+    page = requests.get(url=url, headers=headers)
+    session = HTMLSession()
+    r = session.get(url)
+    r.html.render()
 
-    page = requests.get(url=url)
-    page_result = BeautifulSoup(page.content, 'html.parser')
-    #subPages = page_result.select_one('div[class*="Pagination-"]')
+    page_result = BeautifulSoup(r.html.raw_html, 'html.parser')
+    sub_pages_count = page_result.select('button[class*="navNumberButton"]')
+    for count in range(6, int(sub_pages_count[-1].text)):
+        sub_page_URL = flatListURL + str(count)
+        print(sub_page_URL)
+        sub_page = session.get(sub_page_URL)
+        sub_page.html.render()
+        sub_page_result = BeautifulSoup(sub_page.html.raw_html, 'html.parser')
+        get_flat_data(sub_page_result)
 
-    flats = get_flat_data(page_result)
-    print(flats)
+
     #immoPyDB.insert_many(flats)
